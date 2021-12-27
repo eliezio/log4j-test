166870b71ac8b75e54967b2c4d601f2bfb4c9fbe
Eliezio Oliveira <eliezio.oliveira@teckro.com>
2021-12-27 18:39:04 +0000

    Use -Plog4shell to switch between safe and unsafe modes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.internal.logging.text.StyledTextOutput
+import org.gradle.internal.logging.text.StyledTextOutputFactory
+import org.gradle.kotlin.dsl.support.serviceOf
+
 plugins {
     kotlin("jvm") version "1.6.10"
     application
@@ -21,7 +25,31 @@ dependencies {
     implementation(platform("org.apache.logging.log4j:log4j-bom:$log4jVersion"))
     implementation("com.github.ajalt.clikt:clikt:3.3.0")
 
-    implementation("org.springframework.boot:spring-boot-starter-logging:2.6.1")
+    implementation("org.springframework.boot:spring-boot-starter-logging:2.6.1") {
+        if (project.hasProperty("log4shell")) {
+            exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
+        }
+    }
 
     runtimeOnly("org.apache.logging.log4j:log4j-core")
+}
+
+tasks.named("run") {
+    doLast {
+        val logGroups = setOf("org.apache.logging.log4j", "org.slf4j", "ch.qos.logback")
+        val log4jModules = configurations.runtimeClasspath.get().incoming.resolutionResult.allDependencies
+            .asSequence()
+            .filterIsInstance<ResolvedDependencyResult>()
+            .map { it.selected.id }
+            .filterIsInstance<ModuleComponentIdentifier>()
+            .filter { it.group in logGroups }
+            .map { it.toString() }
+            .toSet()
+        val out = serviceOf<StyledTextOutputFactory>().create("root")
+        out.println()
+        out.withStyle(StyledTextOutput.Style.Identifier).println("Logging modules in runtimeClasspath:")
+        log4jModules.sorted().forEach {
+            out.withStyle(StyledTextOutput.Style.Info).println("+--- $it")
+        }
+    }
 }
